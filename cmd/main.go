70678f7e9f5a99856e8588cc29e81dc57a3e85e0
Jon Sessions <jon@sessionsdev.com>
2024-01-24 00:37:05 -0500

    added open ai integration

--- cmd/main.go
+++ cmd/main.go
@@ -8,24 +8,17 @@ import (
 	"github.com/sessionsdev/blue-octopus/internal/handlers"
 )
 
-	
-
-
 func main() {
     tmplManager := templatemanager.NewTemplateManager("./templates")
 
     http.HandleFunc("/", handlers.ServeHome(tmplManager))
     http.HandleFunc("/api/hello-world", handlers.ServeHelloWorldAPI)
+    http.HandleFunc("/api/generate-text", handlers.GenerateNewText)
 
-    // Handle static files
-    
+    // handle static files
     staticPath := filepath.Join(".", "static")
-    absStaticPath, _ := filepath.Abs(staticPath)
-    fmt.Println("Serving static files from:", absStaticPath)
-
     fs := http.FileServer(http.Dir(staticPath))
     http.Handle("/static/", http.StripPrefix("/static/", fs))
-
 
     fmt.Println("Server is running at http://localhost:8090")
     http.ListenAndServe(":8090", nil)
