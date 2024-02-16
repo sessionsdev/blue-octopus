7b8b40391ded7c3fbc5c6e2dba9f30777940b0f2
Jon Sessions <jon@sessionsdev.com>
2024-02-15 23:34:46 -0500

    activating redis on prod

--- cmd/main.go
+++ cmd/main.go
@@ -7,13 +7,14 @@ import (
 	"path/filepath"
 
 	"github.com/sessionsdev/blue-octopus/internal/handlers"
+	"github.com/sessionsdev/blue-octopus/internal/redis"
 )
 
 func main() {
 	staticPath := filepath.Join(".", "static")
 	handlers.Init(staticPath)
-	// redis.Init()
-	// redis.SetValue("user", "admin", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", 9999999)
+	redis.Init()
+	redis.SetValue("user", "admin", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", 9999999)
 
 	fmt.Println("Server is running at http://localhost:8090")
 	log.Fatal(http.ListenAndServe(":8090", nil))
